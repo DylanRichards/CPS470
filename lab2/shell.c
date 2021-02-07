440e824accfad91b7832ec76fd11b86bcd960bb0
Dylan Richards <dylan.richards81@gmail.com>
2021-02-07 11:05:03 -0800

    Added pipe functionality

--- lab2/shell.c
+++ lab2/shell.c
@@ -18,7 +18,10 @@ void init_command(char *command);
 int get_input(char *command);
 int parse_input(char *args[], char *original_command);
 void run_command(char *args[], int args_num);
-void io_redirection(char *args[], int *args_num);
+void check_pipe(char *args[], int *args_num, char **args2[], int *args_num2);
+int io_redirection(char *args[], int *args_num);
+void close_redirection(int fd);
+void execute_command(char *args[]);
 
 int main(void) {
 	char *args[MAX_ARGS];
@@ -97,23 +100,71 @@ int parse_input(char *args[], char *original_command) {
 }
 
 void run_command(char *args[], int args_num) {
-    pid_t pid = fork();
+    char **args2;
+    int args_num2;
 
+    args_num2 = 0;
+    check_pipe(args, &args_num, &args2, &args_num2);
+
+    pid_t pid = fork();
     if (pid < 0) {
         fprintf(stderr, "Fork Failed\n");
     } else if (pid == 0) {
-        io_redirection(args, &args_num);
-        int err = execvp(args[0], args);
-        if (err == -1) {
-            fprintf(stderr, "Command not found\n");
-            exit(errno);
+        int iofd;
+
+        if (args_num2 == 0) {
+            iofd = io_redirection(args, &args_num);
+            execute_command(args);
+            close_redirection(iofd);
+        } else {
+            int fd[2];
+            pipe(fd);
+
+            pid_t pid2 = fork();
+            if (pid < 0) {
+                fprintf(stderr, "Fork Failed\n");
+            } else if (pid2 == 0) {
+                /* First Command */
+                iofd = io_redirection(args, &args_num);
+                close(fd[0]);
+                dup2(fd[1], STDOUT_FILENO);
+                execute_command(args);
+                close_redirection(iofd);
+                close(fd[1]);
+            } else {
+                /* Second Command */
+                iofd = io_redirection(args2, &args_num2);
+                close(fd[1]);
+                dup2(fd[0], STDIN_FILENO);
+                wait(NULL);
+                execute_command(args2);
+                close_redirection(iofd);
+                close(fd[0]);
+            }
         }
     } else {
         wait(NULL);
     }
 }
 
-void io_redirection(char *args[], int *args_num) {
+void check_pipe(char *args[], int *args_num, char **args2[], int *args_num2) {
+    int i;
+    for (i = 0; i < *args_num; i++) {
+        if (strcmp(args[i], "|") == 0) {
+            free(args[i]);
+            args[i] = NULL;
+
+            *args2 = args + i + 1;
+            *args_num2 = *args_num - i - 1;
+            *args_num = i;
+            break;
+        }
+    }
+}
+
+int io_redirection(char *args[], int *args_num) {
+    int fd = -1;
+
     int i;
     for (i = 0; i < *args_num; i++){
         if (strcmp("<", args[i]) == 0 && i < *args_num - 1) {
@@ -121,12 +172,16 @@ void io_redirection(char *args[], int *args_num) {
 
             mode_t read_mask = S_IRUSR | S_IRGRP | S_IROTH;
 
-            int fd = open(pathname, O_RDONLY, read_mask);
+            fd = open(pathname, O_RDONLY, read_mask);
             dup2(fd, STDIN_FILENO);
             
+            free(args[i]);
+            free(args[i+1]);
             args[i] = NULL;
             args[i+1] = NULL;
             *args_num -= 2;
+
+            break;
         }
         if (strcmp(">", args[i]) == 0 && i < *args_num - 1) {
             char *pathname = args[i+1];
@@ -134,12 +189,30 @@ void io_redirection(char *args[], int *args_num) {
             mode_t read_mask = S_IRUSR | S_IRGRP | S_IROTH;
             mode_t write_mask = S_IWUSR | S_IWGRP | S_IWOTH;
 
-            int fd = open(pathname, O_WRONLY | O_CREAT, read_mask | write_mask);
+            fd = open(pathname, O_WRONLY | O_CREAT, read_mask | write_mask);
             dup2(fd, STDOUT_FILENO);
 
+            free(args[i]);
+            free(args[i+1]);
             args[i] = NULL;
             args[i+1] = NULL;
             *args_num -= 2;
+
+            break;
         }
+    }
+
+    return fd;
+}
+
+void close_redirection(int fd) {
+    if (fd != -1) close(fd);
+}
+
+void execute_command(char *args[]) {
+    int err = execvp(args[0], args);
+    if (err == -1) {
+        fprintf(stderr, "Command not found\n");
+        exit(errno);
     }
 }
